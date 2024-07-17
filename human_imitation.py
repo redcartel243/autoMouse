534d81c64c8ff312fae93d3b84cca9175c9cc9ed
redcartel243 <bukedidavid@gmail.com>
2024-07-17 11:23:25 +0200

    -- Added Mouse Cursor -- Removed prevent_sleep

--- human_imitation.py
+++ human_imitation.py
@@ -5,20 +5,54 @@ import keyboard
 import tkinter as tk
 import random
 import ctypes
+import win32con
+import win32gui
+import atexit
+import win32api
 
 # Flag to control the mouse movement loop
 running = False
 start_time = None
 mouse_thread = None
+keyboard_thread = None
 
 # Disable PyAutoGUI fail-safe
 pyautogui.FAILSAFE = False
 
 # Prevent Windows from sleeping
 def prevent_sleep():
-    ctypes.windll.kernel32.SetThreadExecutionState(
-        ctypes.windll.kernel32.ES_CONTINUOUS | ctypes.windll.kernel32.ES_SYSTEM_REQUIRED
+    win32api.SetThreadExecutionState(
+        win32con.ES_CONTINUOUS | win32con.ES_SYSTEM_REQUIRED
     )
+
+# Save the system cursor before changing it
+def save_current_cursor():
+    cursor = win32gui.LoadImage(0, 32512, win32con.IMAGE_CURSOR, 0, 0, win32con.LR_SHARED)
+    save_system_cursor = ctypes.windll.user32.CopyImage(cursor, win32con.IMAGE_CURSOR, 0, 0, win32con.LR_COPYFROMRESOURCE)
+    return save_system_cursor
+
+saved_cursor = save_current_cursor()
+
+def restore_cursor():
+    # Restore the old cursor
+    print("Restoring cursor")
+    ctypes.windll.user32.SetSystemCursor(saved_cursor, 32512)
+    ctypes.windll.user32.DestroyCursor(saved_cursor)
+
+# Make sure cursor is restored at the end
+atexit.register(restore_cursor)
+
+# Load a custom cursor
+def load_custom_cursor():
+    custom_cursor = win32gui.LoadImage(0, "mouse.cur", win32con.IMAGE_CURSOR, 0, 0, win32con.LR_LOADFROMFILE)
+    if custom_cursor:
+        ctypes.windll.user32.SetSystemCursor(custom_cursor, 32512)
+    else:
+        print("Failed to load custom cursor")
+
+# Reset to the default cursor
+def reset_cursor():
+    restore_cursor()
 
 def simulate_mouse_movement():
     width, height = pyautogui.size()
@@ -35,15 +69,17 @@ def simulate_keyboard_activity():
         time.sleep(random.uniform(10, 30))
 
 def start_program():
-    global running, mouse_thread, keyboard_thread
+    global running, mouse_thread, keyboard_thread, start_time
     if not running:
         running = True
+        start_time = time.time()
         mouse_thread = threading.Thread(target=simulate_mouse_movement)
         keyboard_thread = threading.Thread(target=simulate_keyboard_activity)
         mouse_thread.start()
         keyboard_thread.start()
         update_elapsed_time()
-        prevent_sleep()
+        #prevent_sleep()
+        load_custom_cursor()
 
 def stop_program():
     global running, mouse_thread, keyboard_thread
@@ -52,6 +88,7 @@ def stop_program():
         mouse_thread.join()
     if keyboard_thread and keyboard_thread.is_alive():
         keyboard_thread.join()
+    reset_cursor()
 
 def update_elapsed_time():
     global start_time, running
